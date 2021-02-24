d40b75ac94a7cda53c711adff0b00aaa8fa35f42
Herman Wong <toto23630826@gmail.com>
2021-02-24 12:18:25 +0800

    Added Eigen library for matrix operation for BayesianMatting

--- BayesianMatting.cpp
+++ BayesianMatting.cpp
@@ -3,3 +3,10 @@
 //
 // The cpp file for gaussian filter
 //
+// Reference: https://www.mathworks.com/help/images/ref/fspecial.html#d122e72691
+
+#include "BayesianMatting.h"
+
+std::vector<std::vector<double>> BayesianMatting::getGaussianFilter(int hsize, double sigma) {
+
+}
