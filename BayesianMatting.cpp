5337d7d9d9c7e04cd683a92266d7cf4f9df18d99
Herman Wong <toto23630826@gmail.com>
2021-02-25 20:44:09 +0800

    save progress bayesian matting

--- BayesianMatting.cpp
+++ BayesianMatting.cpp
@@ -14,8 +14,11 @@
 #include <vector>
 
 using namespace std;
+using Eigen::MatrixXd;
+using Eigen::Vector3d;
+using Eigen::VectorXd;
 
-void BayesianMatting::loadImg(char* iname)
+bool BayesianMatting::initialize(char* iname)
 {
 	// try to open the image to read
 	unsigned char* data;
@@ -24,39 +27,101 @@ void BayesianMatting::loadImg(char* iname)
 	if ((data = readBMP(iname, w, h)) == NULL)
 	{
 		fl_alert("Can't load bitmap file");
-		return;
+		return false;
 	}
+
+	// INITIALIZING ALL DATA MEMBERS
+	height = h;
+	width = w;
+
+	// Create gray Img
+	unsigned char* grayData = new unsigned char[height * width * 3];
+	for (int i = 0; i < height * width; i++) {
+		grayData[i] = min(255, max(data[i * 3] * 299 / 1000 + data[i * 3 + 1] * 587 / 1000 + data[i * 3 + 2] * 114 / 1000, 0));
+	}
+
+	img.resize(height * width, 3);									// store the original image			**THIS IS A MATRIX STORING COLOR ALSO
+	foreground.resize(height * width, 3);							// store the F						**THIS IS A MATRIX STORING COLOR ALSO
+	background.resize(height * width, 3);							// store the B						**THIS IS A MATRIX STORING COLOR ALSO
+
+	alphamap.resize(height * width);								// store the alpha area which to be determinded		**THIS IS A STD::VECTOR
+	nUnknownPixel = 0;
 	
-	this->width = w;
-	this->height = h;
-
-
-	for (int i = 0; i < height; i++) {
-		vector<vector<double>> fgRow;
-		vector<vector<double>> bgRow;
-		vector<vector<double>> alphaRow;
-
-		for (int j = 0; j < width; j++) {
-			vector<double> color;
-			for (int c = 0; c < 3; c++) {
-				color.push_back(data[i * width + j + c]/255.f);
-			}
-			if (data[i * width + j] == 255) {
-				fgRow.push_back(color);
-				bgRow.push_back({ 0, 0, 0 });
-			}
+	Vector3d zeroPixel;
+	zeroPixel << 0, 0, 0;
+	for (int i = 0; i < height * width; i++) {
+		Vector3d pixel;												// this is the rgb pixel;
+		pixel << data[i * 3] / 255.f, data[i * 3 + 1] / 255.f, data[i * 3 + 2] / 255.f;
+		img.row(i) = pixel;											// store the original image
+		if (grayData[i] == 255) {				// F
+			foreground.row(i) = pixel;
+			background.row(i) = zeroPixel;
+			alphamap[i] = 1;
+		}
+		else if (grayData[i] == 0) {			// B
+			foreground.row(i) = zeroPixel;
+			background.row(i) = pixel;
+			alphamap[i] = 0;
+		}
+		else {									// Alpha
+			alphamap[i] = NAN;
+			nUnknownPixel++;
 		}
 	}
-
-
 	delete[] data;
+	delete[] grayData;
+	return true;
 }
 
-double BayesianMatting::solve(double sigma, int nearest, int minNearest) {
-	return 1;
+void BayesianMatting::solve(char* iname, double sigma, int nearest, int minNearest) {
+	if (!initialize(iname)) {
+		return;
+	}
+	vector<int> unknownPixel;					// store the x, y of unknown pixels --> pixelX = i % width, pixelY = i / width;
+	for (int i = 0; i < alphamap.size(); i++) {
+		if (alphamap[i] != 0 && alphamap[i] != 1) {
+			unknownPixel.push_back(i);
+		}
+	}
+	
+	while (!unknownPixel.empty()) {
+		int infiniteLoop = unknownPixel.size();
+		for (int iter = 0; iter != unknownPixel.size(); iter++) {
+			int pixelX = unknownPixel[iter] % width;
+			int pixelY = unknownPixel[iter] / width;
+
+			VectorXd a = getNearestVector(alphamap, pixelX, pixelY);
+			MatrixXd F = getNearestMatrix(foreground, pixelX, pixelY);
+			VectorXd FWeight = getFWeight(a);
+			MatrixXd B = getNearestMatrix(background, pixelX, pixelY);
+			VectorXd BWeight = getBWeight(a);
+
+			// todo if len Fweight < min weight<min continue;
+
+			double alphaRes;
+			VectorXd FRes(3), BRes(3), C(3);
+			C = img.row(iter);
+
+			solveFBAlpha();
+
+			foreground.row(iter) = FRes;
+			background.row(iter) = BRes;
+			alphamap[iter] = alphaRes;
+			unknownPixel.erase(unknownPixel.begin() + iter);
+
+		}
+		if (unknownPixel.size() == infiniteLoop)
+			minNearest *= 0.9;
+	}
+	// todo output files
 }
 
-vector<vector<double>> BayesianMatting::getGaussianFilter(int hsize, double sigma) {
+void BayesianMatting::solveFBAlpha(double meanF, double sigmaF, double meanB, double sigmaB, double alpha_init, double C, double sigmaC, int maxIter, double minLike) {
+
+}
+
+VectorXd BayesianMatting::getGaussianFilter(int hsize, double sigma) {
+	VectorXd gaussian(hsize * hsize);				//gaussian
 	vector<vector<double>> filter;
 	double sum = 0;
 	for (int i = 0; i < hsize; i++) {
@@ -65,17 +130,41 @@ vector<vector<double>> BayesianMatting::getGaussianFilter(int hsize, double sigm
 			double val = exp(-((i - hsize / 2) * (i - hsize / 2) + (j - hsize / 2) * (j - hsize / 2)) / (2 * sigma * sigma));
 			sum += val;
 			v.push_back(val);
+			gaussian[i * hsize + j] = val;			//gaussian
 		}
 		filter.push_back(v);
 	}
 	for (int i = 0; i < hsize; i++) {
 		for (int j = 0; j < hsize; j++) {
 			filter[i][j] /= sum;
+			gaussian[i * hsize + j] /= sum;			//gaussian
 		}
 	}
-	return filter;
+	return gaussian;
 }
 
-void BayesianMatting::solveFBAlpha(double meanF, double sigmaF, double meanB, double sigmaB, double alpha_init, double C, double sigmaC, int maxIter, double minLike) {
+MatrixXd BayesianMatting::getNearestMatrix(MatrixXd m, int x, int y, int size = BAYESIAN_NUMBER_NEAREST) {
 
+}
+
+VectorXd BayesianMatting::getNearestVector(VectorXd v, int x, int y, int size = BAYESIAN_NUMBER_NEAREST) {
+
+}
+
+VectorXd BayesianMatting::getFWeight(VectorXd v) {
+	VectorXd gW = getGaussianFilter();
+	VectorXd res(gW.size());
+	for (int i = 0; i < gW.size(); i++) {
+		res[i] = v[i] * v[i] * gW[i];
+	}
+	return res;
+}
+
+VectorXd BayesianMatting::getBWeight(VectorXd v) {
+	VectorXd gW = getGaussianFilter();
+	VectorXd res(gW.size());
+	for (int i = 0; i < gW.size(); i++) {
+		res[i] = (1 - v[i]) * (1 - v[i]) * gW[i];
+	}
+	return res;
 }
--- BayesianMatting.h
+++ BayesianMatting.h
@@ -3,6 +3,39 @@
 //
 // The header file for BayesianMatting
 //
+// Please note that this BayesianMatting implementation is inspired by https://github.com/liquidmetal/Bayesian-Matting and https://github.com/MarcoForte/bayesian-matting
+
+//-------------------------------------------------------------------------------------------------
+//step one : find the trimap[LOAD]
+//
+//find the backImgand ForeImg by mask
+//we need to know the "gray" area.This is denoted by alphaImg.
+//for alphaImg, the area of backImg is 0 and 1 for foreimg.yet to be determined for alphaImg
+//
+//step two : solve the solution for each pixel to get result[SOLVE]
+//
+//we do step 3 to step 4 here
+//
+//step three : apply guassian kernel in nearerst pixel[each pixel loop]
+//
+//note that the gaussian kernel needs to divide its max element
+//keep a record of how many pixels in total and how many pixels which are alpha state
+//for each pixel, we save the wf, wb in flist and blist
+//get the mean alpha here, mean alpha only counts those pixels which are in alpha state
+//
+//step four : create the Fand B
+//
+//Fand B are matrixand only include the nearest pixeland only include those of alpha state
+//make two vector Fw, Bw, to store the point wFand wB
+//By clustering, we obtain meanF, covF, meanB, covB
+//
+//step five : solving alpha, fand b
+//
+//to solve this, we need the 	mean alpha, meanF, covF, mean B, covB
+//Then we do the iterative alogrithm to get the maximum likelihoodand update F Band alpha
+//-------------------------------------------------------------------------------------------------
+
+
 #ifndef BAYESIANMATTING_H
 #define BAYESIANMATTING_H
 
@@ -14,30 +47,37 @@
 #define BAYESIAN_MIN_LIKE				1e-6
 
 #include <Eigen/Dense>
-#include <vector>
 
-using namespace std;
 using Eigen::MatrixXd;
+using Eigen::VectorXd;
 
 class BayesianMatting {
 public:
 	BayesianMatting() = default;
 	~BayesianMatting() = default;
-	double solve(double sigma = BAYESIAN_SIGMA, int nearest = BAYESIAN_NUMBER_NEAREST, int minNearest = BAYESIAN_MIN_NUMBER_NEAREST);
+	void solve(char* iname, double sigma = BAYESIAN_SIGMA, int nearest = BAYESIAN_NUMBER_NEAREST, int minNearest = BAYESIAN_MIN_NUMBER_NEAREST);
 
 private:
-	void loadImg(char* iname);
-	std::vector<std::vector<double>> getGaussianFilter(int size, double sigma);
-	void solveFBAlpha(double meanF, double sigmaF, double meanB, double sigmaB, double alpha_init, double C, double sigmaC = BAYESIAN_SIGMA_C, int maxIter = BAYESIAN_MAX_ITER, double minLike = BAYESIAN_MIN_LIKE);
-	
-	int height;
-	int width;
+	// loadImg would create a black white version of the img and load the necessary data into img, foreground, background, alphamap and unknownPixel;
+	bool initialize(char* iname);
+	void solveFBAlpha(double meanF, double sigmaF, double meanB, double sigmaB, double alpha_init, VectorXd C, double sigmaC = BAYESIAN_SIGMA_C, int maxIter = BAYESIAN_MAX_ITER, double minLike = BAYESIAN_MIN_LIKE);
+	VectorXd getGaussianFilter(int size = BAYESIAN_NUMBER_NEAREST, double sigma = BAYESIAN_SIGMA);
+	MatrixXd getNearestMatrix(MatrixXd m, int x, int y, int size = BAYESIAN_NUMBER_NEAREST);
+	VectorXd getNearestVector(VectorXd v, int x, int y, int size = BAYESIAN_NUMBER_NEAREST);
+	VectorXd getFWeight(VectorXd v);
+	VectorXd getBWeight(VectorXd v);
 
-	MatrixXd img;
-	MatrixXd fgImg;
-	MatrixXd bgImg;
-	MatrixXd trimap;
-	MatrixXd alphamap;
+	// DATA MEMBERS
+	int height;			// y
+	int width;			// x
+
+	MatrixXd		img;
+	MatrixXd		foreground;
+	MatrixXd		background;
+	VectorXd		alphamap;
+
+	int				nUnknownPixel;
+	int				min
 };
 
 #endif
